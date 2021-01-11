2509a7619ac1c1a3c618dcf8a53d30dccb112e2e
fr1x3 <gathufx@gmail.com>
2021-01-11 18:50:02 +0300

    ui completed

---
+++ app/src/main/java/com/fr1x3/cryptocurrency/ui/CryptoListFragment.kt
@@ -0,0 +1,33 @@
+package com.fr1x3.cryptocurrency.ui
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.fr1x3.cryptocurrency.R
+import com.fr1x3.cryptocurrency.viewmodels.CryptoListViewModel
+
+class CryptoListFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = CryptoListFragment()
+    }
+
+    private lateinit var viewModel: CryptoListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.crypto_list_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(CryptoListViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/fr1x3/cryptocurrency/viewmodels/CryptoListViewModel.kt
@@ -0,0 +1,7 @@
+package com.fr1x3.cryptocurrency.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class CryptoListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
